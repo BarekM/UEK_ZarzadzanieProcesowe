8a0375c08b343d85573e3c99377154a4215d3372
BarekM <35003350+BarekM@users.noreply.github.com>
2019-01-21 10:12:27 +0100

    Initial commit

---
+++ Temat1_KlasyfikacjaCelow/System-zarządzania-produkcją-na-dziale-podmontażu-podwozia.mm
@@ -0,0 +1,169 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1542880540692" ID="ID_1254414184" MODIFIED="1542881434560" TEXT="System zarz&#x105;dzania produkcj&#x105; na dziale podmonta&#x17c;u podwozia">
+<node CREATED="1542880620007" ID="ID_119857522" MODIFIED="1542881046211" POSITION="right" TEXT="Cel g&#x142;&#xf3;wny: Poprawa wska&#x17a;nik&#xf3;w KPI">
+<icon BUILTIN="full-1"/>
+<node CREATED="1542880696158" ID="ID_1945488444" MODIFIED="1542881053039" TEXT="Poprawa jako&#x15b;ci produkt&#xf3;w">
+<icon BUILTIN="full-2"/>
+<node CREATED="1542880740576" ID="ID_1355393385" MODIFIED="1542882153342" TEXT="Optymalizacja procesu produkcyjnego">
+<icon BUILTIN="full-3"/>
+<node CREATED="1542883294103" ID="ID_629948464" MODIFIED="1542884944897" TEXT="Zg&#x142;aszanie wniosk&#xf3;w KAIZEN">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542883309052" ID="ID_902416211" MODIFIED="1542884947710" TEXT="Przegl&#x105;d okresowy proces&#xf3;w produkcyjnych">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+<node CREATED="1542881573488" ID="ID_1998870708" MODIFIED="1542882156591" TEXT="Szkolenia pracownik&#xf3;w">
+<icon BUILTIN="full-3"/>
+<node CREATED="1542882037134" ID="ID_1721140496" MODIFIED="1542882168091" TEXT="Cykliczne szkolenia stanowiskowe">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542882053503" ID="ID_1030135011" MODIFIED="1542882170685" TEXT="Wst&#x119;pne szkolenia stanowiskowe">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542882070019" ID="ID_331674547" MODIFIED="1542882174450" TEXT="Dodatkowe szkolenia dla pracownik&#xf3;w z du&#x17c;&#x105; ilo&#x15b;ci&#x105; b&#x142;ed&#xf3;w">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542885319559" ID="ID_1660066409" MODIFIED="1542885357806" TEXT="Okre&#x15b;lenie oczekiwa&#x144; szkoleniowych">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542886625612" ID="ID_1100558716" MODIFIED="1542886739766" TEXT="Wsp&#xf3;&#x142;praca ze szko&#x142;ami  i uczelniami o podobnym profilu ">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+<node CREATED="1542881585723" ID="ID_1905631293" MODIFIED="1542882159701" TEXT="Informowanie o wynik&#x142;ych b&#x142;&#x119;dach">
+<icon BUILTIN="full-3"/>
+<node CREATED="1542881731419" ID="ID_1448314355" MODIFIED="1542882176997" TEXT="Om&#xf3;wienie na zebraniach porannych">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542881971608" ID="ID_1773694691" MODIFIED="1542882180169" TEXT="karty b&#x142;edw na tablicach dzia&#x142;owych">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542882018617" ID="ID_256015658" MODIFIED="1542884953178" TEXT="Szybkie informacje stanowiskowe w formie zdj&#x119;&#x107; i kr&#xf3;tkiego opisu">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+<node CREATED="1542881755569" ID="ID_1008505908" MODIFIED="1542882162920" TEXT="Sta&#x142;y monitoring jako&#x15b;ci produktu">
+<icon BUILTIN="full-3"/>
+<node CREATED="1542881792673" ID="ID_1137638992" MODIFIED="1542882183403" TEXT="Opracowanie systemu monitoringu b&#x142;ed&#xf3;w">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542881815062" ID="ID_1817234288" MODIFIED="1542882185997" TEXT="Stworzenie bramek jako&#x15b;ciowych">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542881844394" ID="ID_183336363" MODIFIED="1542882188856" TEXT="Informacje z servis&#xf3;w zewnetrznych">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542881874781" ID="ID_1435202514" MODIFIED="1542882192403" TEXT="Spotkania kontroler&#xf3;w jako&#x15b;ci">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+</node>
+<node CREATED="1542880759910" ID="ID_1856198888" MODIFIED="1542881057148" TEXT="Zwi&#x119;kszenie produkcji">
+<icon BUILTIN="full-2"/>
+<node CREATED="1542880915129" ID="ID_231544906" MODIFIED="1542881123329" TEXT="Szybsze dostarczanie produktu na nastepne stanowisko">
+<icon BUILTIN="full-3"/>
+<node CREATED="1542886555499" ID="ID_844760160" MODIFIED="1542886745485" TEXT="Modyfikacja &#x142;a&#x144;cucha dostaw">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+<node CREATED="1542881613426" ID="ID_313296709" MODIFIED="1542882150232" TEXT="Eliminacja zb&#x119;dnych materia&#x142;&#xf3;w na stanowisku pracy">
+<icon BUILTIN="full-3"/>
+<node CREATED="1542885992871" ID="ID_262130323" MODIFIED="1542886175294" TEXT="Zmniejszenie drogi przej&#x15b;&#x107;">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542886020053" ID="ID_325190883" MODIFIED="1542886177778" TEXT="Zmniejszenie mo&#x17c;liwo&#x15b;ci pobrania niew&#x142;a&#x15b;ciwej cz&#x119;&#x15b;ci">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542886090546" ID="ID_1655992266" MODIFIED="1542886180825" TEXT="Zwi&#x119;kszenie obszaru na dodatkowe nowe cz&#x119;&#x15b;ci">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542886506866" ID="ID_728556944" MODIFIED="1542886748391" TEXT="Korekta stanu obecnego">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+<node CREATED="1542882941696" ID="ID_1564584007" MODIFIED="1542883155907" TEXT="Eliminacja zb&#x119;dnych proces&#xf3;w">
+<icon BUILTIN="full-3"/>
+<node CREATED="1542883004002" ID="ID_1492861799" MODIFIED="1542883272324" TEXT="Zbli&#x17c;enie stanowisk podmontazowych do lini produkcyjnej">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542883036825" ID="ID_437708485" MODIFIED="1542883275012" TEXT="&#x141;&#x105;czenie proces&#xf3;w nie obcia&#x17c;onych w 100%">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542883111408" ID="ID_1467064263" MODIFIED="1542883278418" TEXT="Optymalne przybli&#x17c;enie cz&#x119;sci cz&#x119;&#x15b;ciej u&#x17c;ywanych do pracownika">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542883374616" ID="ID_630501556" MODIFIED="1542884958975" TEXT="Zmniejszenie czas&#xf3;w maszynowych-automatyzacja">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+<node CREATED="1542883212091" ID="ID_240275994" MODIFIED="1542883268887" TEXT="Delegowanie dodatkowych pracownik&#xf3;w na stanowiska mocno obci&#x105;&#x17c;one ">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+<node CREATED="1542880816280" ID="ID_98681315" MODIFIED="1542881060367" TEXT="Eliminowanie strat">
+<icon BUILTIN="full-2"/>
+<node CREATED="1542882507039" ID="ID_23103747" MODIFIED="1542883159501" TEXT="Blokowanie cz&#x119;&#x15b;ci niezgodnych">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1542882523743" ID="ID_1390705215" MODIFIED="1542883162714" TEXT="Raportowanie niezgodno&#x15b;ci prze&#x142;o&#x17c;onemu">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1542882561445" ID="ID_1218259473" MODIFIED="1542886475505" TEXT="Reklamacje do dostawc&#xf3;w"/>
+</node>
+<node CREATED="1542881133433" ID="ID_1063035999" MODIFIED="1542881235463" TEXT="Zmniejszenie koszt&#xf3;w produkcji">
+<icon BUILTIN="full-2"/>
+<node CREATED="1542882240075" ID="ID_721053931" MODIFIED="1542883165839" TEXT="Zmiana dostawcy materia&#x142;&#xf3;w oko&#x142;o produkcyjnych">
+<icon BUILTIN="full-3"/>
+<node CREATED="1542886327459" ID="ID_7952257" MODIFIED="1542886809859" TEXT="Negocjacja um&#xf3;w z nowymi dostawcami">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542886824473" ID="ID_1888971076" MODIFIED="1542887802148" TEXT="Nawi&#x105;zanie wsp&#xf3;&#x142;pracy z alternatywnym dostawc&#x105;">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+<node CREATED="1542882414143" ID="ID_410481937" MODIFIED="1542883169574" TEXT="Wsp&#xf3;lna platforma do zamawiania narz&#x119;dzi">
+<icon BUILTIN="full-3"/>
+<node CREATED="1542885101907" ID="ID_1572569497" MODIFIED="1542885437837" TEXT="zamawianie wi&#x119;kszej ilo&#x15b;ci dla kilku dzia&#x142;&#xf3;w">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542885418681" ID="ID_146224917" MODIFIED="1542885440806" TEXT="Oszcz&#x119;dno&#x15b;ci na kosztach przesy&#x142;ki">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542886376055" ID="ID_1827837997" MODIFIED="1542886805937" TEXT="Eliminacja zbednych posrednik&#xf3;w">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+<node CREATED="1542882461565" ID="ID_1410820532" MODIFIED="1542884967391" TEXT="Absencja pracownicza">
+<icon BUILTIN="full-3"/>
+<node CREATED="1542884515734" ID="ID_843624226" MODIFIED="1542884970579" TEXT="Zmniejszenie wska&#x17a;nika chorobowo&#x15b;ci">
+<icon BUILTIN="full-4"/>
+<node CREATED="1542884561584" ID="ID_1538454127" MODIFIED="1542884980172" TEXT="Tworzenie ergonomicznych stanowisk pracy">
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1542884581253" ID="ID_1223942455" MODIFIED="1542884983110" TEXT="Tworzenie grafiku rotacji stanowiskowej">
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1542884816103" ID="ID_1882977960" MODIFIED="1542884985656" TEXT="Optymalizacja stanowisk narazonych na du&#x17c;e wachania temperatury ">
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+<node CREATED="1542884605747" ID="ID_1732711234" MODIFIED="1542884973547" TEXT="Stworzenie systemu premiowania">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1542884718378" ID="ID_765556183" MODIFIED="1542884976360" TEXT="Tworzenie plan&#xf3;w urlopowych">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1542881440720" ID="ID_449664570" MODIFIED="1542885569797" POSITION="left" TEXT="Dotychczasowy cel g&#x142;&#xf3;wny: Utrzymanie dziennego limitu produkcji">
+<icon BUILTIN="full-1"/>
+<node CREATED="1542885575027" ID="ID_439774460" MODIFIED="1542886273588" TEXT="Produktywno&#x15b;ci"/>
+<node CREATED="1542885886439" ID="ID_1304297554" MODIFIED="1542885967469" TEXT="Eliminacja przestoj&#xf3;w"/>
+<node CREATED="1542885970574" ID="ID_230661904" MODIFIED="1542886265588" TEXT="Absencja pracownicza"/>
+</node>
+</node>
+</map>
